982a7ae57e9c004347353d8ffbe0280076f16dcb
SHIVPRASAD SACHINDEO BENDRE <shivprasad.sachindeo-bendre@capgemini.com>
2025-05-07 15:59:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello World Python v2"
+    return "Hello World Python v3"
 
 if __name__== "__main__":
     app.run(host="0.0.0.0",port=3000)
